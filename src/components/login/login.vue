fc31cb71f114d5c213af8519cac9c736d383b8e4
mmz <372461239@qq.com>
2019-04-20 11:25:42 +0800

    在没有element—ui组件之前的修改

---
+++ src/components/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="form-position">
+    <div class="titleinfo">停车场信息管理系统</div>
+    <userinput></userinput>
+  </div>
+</template>
+
+<script>
+import userinput from './userinput'
+export default {
+  name: 'login',
+  components: {
+    userinput
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/login/userinput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      我是userinput
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'userinput'
+}
+</script>
+
+<style scoped>
+
+</style>
